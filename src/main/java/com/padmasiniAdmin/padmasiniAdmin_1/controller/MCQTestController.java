3e90d48f8f90b1d185be5e0063de128101b5a997
naveen-127 <naveen@padmasini.com>
2025-09-29 15:43:56 +0530

    Update MCQTestController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/MCQTestController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/MCQTestController.java
@@ -5,12 +5,21 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import java.util.Collections;
+
+import com.padmasiniAdmin.padmasiniAdmin_1.service.MCQTestService;
+import com.padmasiniAdmin.padmasiniAdmin_1.model.WrapperMCQTest;
+
 
 import com.padmasiniAdmin.padmasiniAdmin_1.model.WrapperMCQTest;
 import com.padmasiniAdmin.padmasiniAdmin_1.service.MCQTestService;
