288ffef9b63c626c88561a047b4db2f5d1593093
naveen-127 <naveen@padmasini.com>
2025-10-05 00:39:13 +0530

    Update WrapperUnitRequest.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/model/WrapperUnitRequest.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/model/WrapperUnitRequest.java
@@ -1,31 +1,25 @@
 package com.padmasiniAdmin.padmasiniAdmin_1.model;
 
 public class WrapperUnitRequest {
-private String dbname;
-private UnitRequest unit;
-private String subjectName;
+    private String dbname;
+    private String rootId;
+    private String subjectName;
+    private String unitName;
+    private String standard; // if used anywhere
 
-public String getSubjectName() {
-	return subjectName;
-}
-public void setSubjectName(String subjectName) {
-	this.subjectName = subjectName;
-}
-public String getDbname() {
-	return dbname;
-}
-public void setDbname(String dbname) {
-	this.dbname = dbname;
-}
-public UnitRequest getUnit() {
-	return unit;
-}
-public void setUnit(UnitRequest unit) {
-	this.unit = unit;
-}
-@Override
-public String toString() {
-	return "WrapperUnitRequest [dbname=" + dbname + ", unit=" + unit + "]";
-}
+    // Getters & Setters
+    public String getDbname() { return dbname; }
+    public void setDbname(String dbname) { this.dbname = dbname; }
 
+    public String getRootId() { return rootId; }
+    public void setRootId(String rootId) { this.rootId = rootId; }
+
+    public String getSubjectName() { return subjectName; }
+    public void setSubjectName(String subjectName) { this.subjectName = subjectName; }
+
+    public String getUnitName() { return unitName; }
+    public void setUnitName(String unitName) { this.unitName = unitName; }
+
+    public String getStandard() { return standard; }
+    public void setStandard(String standard) { this.standard = standard; }
 }
