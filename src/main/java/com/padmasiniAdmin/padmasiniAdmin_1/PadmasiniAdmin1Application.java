bbd53ea19dc93c81015b37210ee6b647f73f4235
naveen-127 <naveen@padmasini.com>
2025-10-07 12:47:09 +0530

    Update PadmasiniAdmin1Application.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/PadmasiniAdmin1Application.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/PadmasiniAdmin1Application.java
@@ -13,24 +13,18 @@ public class PadmasiniAdmin1Application {
         SpringApplication.run(PadmasiniAdmin1Application.class, args);
     }
 
-   @Bean
-public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                .allowedOriginPatterns(
-                    "http://localhost:5173",
-                    "https://d2kr3vc90ue6me.cloudfront.net",
-                    "https://trilokinnovations.com",
-                    "https://www.trilokinnovations.com",
-                    "https://padmasini7-frontend.netlify.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-        }
-    };
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {   
+         return new WebMvcConfigurer() {
+            @Override	
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173","https://d2kr3vc90ue6me.cloudfront.net","https://trilokinnovations.com","https://www.trilokinnovations.com", "https://padmasini7-frontend.netlify.app")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")    
+                    .allowCredentials(true);
+            }
+        };
+    }
 }
-
-}
+   
