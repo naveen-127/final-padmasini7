6a38247e8a45db0fe7b4361ec7b9ac4439678d21
naveen-127 <naveen@padmasini.com>
2025-11-07 16:38:25 +0530

    Update SignINController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/SignINController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/SignINController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.padmasiniAdmin.padmasiniAdmin_1.manageUser.UserModel;
@@ -23,6 +24,7 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.Session;
  
 @RestController
+@RequestMapping("/api")
 public class SignINController {
 	
 	@Autowired 
