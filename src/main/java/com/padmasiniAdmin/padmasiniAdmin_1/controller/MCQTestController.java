4a503a573b575c50f9079532d11331d5de4256fb
naveen-127 <naveen@padmasini.com>
2025-11-10 14:52:49 +0530

    Update MCQTestController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/MCQTestController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/MCQTestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ import com.padmasiniAdmin.padmasiniAdmin_1.model.WrapperMCQTest;
 import com.padmasiniAdmin.padmasiniAdmin_1.service.MCQTestService;
 
 @RestController
+@RequestMapping("/api")
 public class MCQTestController {
 	@Autowired
 	MCQTestService mcqTestService;
