19a9fa6427bd6b247ac500de9d4ce3edf41e9a49
naveen-127 <naveen@padmasini.com>
2026-01-02 16:30:39 +0530

    Update UnitDataController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/student/UnitDataController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/student/UnitDataController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.padmasiniAdmin.padmasiniAdmin_1.model.UnitRequest;
@@ -18,7 +19,7 @@ import com.padmasiniAdmin.padmasiniAdmin_1.service.studentService.StudentDataSer
 
 
 @RestController
-// @RequestMapping("/api")
+@RequestMapping("/api")
 public class UnitDataController {
 	
 	@Autowired
