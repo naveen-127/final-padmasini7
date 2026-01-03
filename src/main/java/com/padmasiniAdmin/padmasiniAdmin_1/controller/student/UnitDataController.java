d3bd5d11617e03228ae747c7aef35708ab2de8fb
naveen-127 <naveen@padmasini.com>
2026-01-03 10:47:29 +0530

    Update UnitDataController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/student/UnitDataController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/student/UnitDataController.java
@@ -1,13 +1,10 @@
+// File: /home/ec2-user/final-padmasini7/src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/student/UnitDataController.java
 package com.padmasiniAdmin.padmasiniAdmin_1.controller.student;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.padmasiniAdmin.padmasiniAdmin_1.model.UnitRequest;
 import com.padmasiniAdmin.padmasiniAdmin_1.model.WrapperUnit;
-import com.padmasiniAdmin.padmasiniAdmin_1.service.studentService.StudentDataService;
-import com.padmasiniAdmin.padmasiniAdmin_1.service.StudentDataService; 
-
+import com.padmasiniAdmin.padmasiniAdmin_1.service.StudentDataService; // Correct import
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UnitDataController {
-	
-	@Autowired
-	private StudentDataService data;
-@PostMapping("/getUnitDataForStandrard/{standardType}")
-public List<UnitRequest> getUnitDataForStudent(@PathVariable("standardType") String subject,@RequestBody WrapperUnit request){
-	List<UnitRequest> unitData=data.getUnitData(subject,request);
-	System.out.println(unitData);
-	
-	return unitData;
-}
+    
+    @Autowired
+    private StudentDataService service;
+    
+    @PostMapping("/getUnits")
+    public List<UnitRequest> getUnits(@RequestBody WrapperUnit request) {
+        return service.getUnitData(request.getSubjectName(), request);
+    }
 }
