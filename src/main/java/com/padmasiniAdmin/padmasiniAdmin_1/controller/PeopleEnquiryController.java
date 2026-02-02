68b5e69ab73e27d3c1b34c759783ca3f395a8011
naveen-127 <naveen@padmasini.com>
2026-02-02 12:45:47 +0530

    Update PeopleEnquiryController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/PeopleEnquiryController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/PeopleEnquiryController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/people-enquiries")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "https://trilokinnovations.com"}, allowCredentials = "true")
 public class PeopleEnquiryController {
     
     private static final Logger logger = LoggerFactory.getLogger(PeopleEnquiryController.class);
