44587a5770c639c4146016a6e4b09587e486d391
naveen-127 <naveen@padmasini.com>
2026-02-02 12:37:32 +0530

    Update PeopleEnquiryController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/PeopleEnquiryController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/PeopleEnquiryController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/people-enquiries")
+@RequestMapping("/people-enquiries")
 @CrossOrigin(origins = "https://dafj1druksig9.cloudfront.net", allowCredentials = "true")
 public class PeopleEnquiryController {
     
