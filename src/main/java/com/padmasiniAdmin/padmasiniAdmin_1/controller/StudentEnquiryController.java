cbcbbe4f47cd7bfd73ce2655ace78db321af38b8
naveen-127 <naveen@padmasini.com>
2026-02-02 12:28:44 +0530

    Update StudentEnquiryController.java

--- src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/StudentEnquiryController.java
+++ src/main/java/com/padmasiniAdmin/padmasiniAdmin_1/controller/StudentEnquiryController.java
@@ -26,7 +26,7 @@ import org.bson.types.Binary;
 
 @RestController
 @RequestMapping("/api/support")
-@CrossOrigin(origins = "https://dafj1druksig9.cloudfront.net/", allowCredentials = "true")
+@CrossOrigin(origins = "https://trilokinnovations.com", allowCredentials = "true")
 public class StudentEnquiryController {
     
     private static final Logger logger = LoggerFactory.getLogger(StudentEnquiryController.class);
